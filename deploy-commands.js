3222fe2f72954a9899ae882324bb227294dea6bd
ddbgbdbdauykbydieagywdg8owea <twinvrod@gmail.com>
2026-09-23 17:25:00 -0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -19,6 +19,26 @@ const commands = [
     .setDescription('View the top 100 for a challenge')
     .addStringOption(opt =>
       opt.setName('id').setDescription('Challenge ID').setRequired(true).setAutocomplete(true)),
+  new SlashCommandBuilder()
+    .setName('top7leaderboard')
+    .setDescription('Leaderboard ranked by number of top 7 finishes')
+    .addIntegerOption(opt =>
+      opt.setName('page').setDescription('Page number').setMinValue(1)),
+  new SlashCommandBuilder()
+    .setName('wrleaderboard')
+    .setDescription('Leaderboard ranked by number of world records')
+    .addIntegerOption(opt =>
+      opt.setName('page').setDescription('Page number').setMinValue(1)),
+  new SlashCommandBuilder()
+    .setName('avg')
+    .setDescription('Leaderboard ranked by best average rank (top 100 global players only)')
+    .addIntegerOption(opt =>
+      opt.setName('page').setDescription('Page number').setMinValue(1)),
+  new SlashCommandBuilder()
+    .setName('improve')
+    .setDescription('Shows which challenges a player has no top 100 entry on')
+    .addStringOption(opt =>
+      opt.setName('name').setDescription('Player name').setRequired(true).setAutocomplete(true)),
 ].map(c => c.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
